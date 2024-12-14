f4028d4d3520456f876bdde9733f069d97bce068
shiran <2488252513@qq.com>
2024-12-14 21:18:13 +0800

    修改配置文件写入方式为.env文件，并新增配置初始化方法

--- setting.py
+++ setting.py
@@ -1,6 +1,22 @@
-wg_name = "wg0"
-wg_config_path = "/etc/wireguard/wg0.conf"
-wg_host = ""
-Token = "111111111111"
-user_name = "admin"
-user_password = "admin"
+import shutil
+
+from dotenv import load_dotenv
+import os
+
+
+template_file = ".env.example"
+target_file = ".env"
+if not os.path.exists(target_file):
+    shutil.copy(template_file, target_file)
+    print(f"初始化 {template_file} 文件，请修改里面的配置项再运行程序.")
+    exit()
+else:
+    print(f"{target_file} already exists.")
+
+load_dotenv()
+wg_name = os.getenv("WG_NAME", "wg0")
+wg_config_path = os.getenv("WG_CONFIG_PATH", "/etc/wireguard/wg0.conf")
+wg_host = os.getenv("WG_HOST", "")
+Token = os.getenv("TOKEN", "")
+user_name = os.getenv("USER_NAME", "")
+user_password = os.getenv("USER_PASSWORD", "")
